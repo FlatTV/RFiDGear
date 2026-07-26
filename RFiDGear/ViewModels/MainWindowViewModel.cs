7523e702abf7651c7c7408aea5aa8134e1e25a5b
FlatTV <FlatTV@gmx.de>
2026-07-26 14:47:56 +0200

    Mark my edition

--- RFiDGear/ViewModels/MainWindowViewModel.cs
+++ RFiDGear/ViewModels/MainWindowViewModel.cs
@@ -1738,7 +1738,7 @@ namespace RFiDGear.ViewModel
             Application.Current.MainWindow.Activated -= new EventHandler(LoadCompleted);
 
             mw = (MainWindow)Application.Current.MainWindow;
-            mw.Title = string.Format("RFiDGear v{0}.{1}.{2}", Version.Major, Version.Minor, Version.Build);
+            mw.Title = string.Format("RFiDGear v{0}.{1}.{2} bam5br edition", Version.Major, Version.Minor, Version.Build);
 
             updateScheduler.Begin(() => AskForUpdateNow());
             readerMonitor.StartMonitoring(CheckReader);
